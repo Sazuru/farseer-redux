84ca8880f4ba5f732adb023ae0da08d1abc819d6
Valentin Ageev <sazuru@gmail.com>
2020-02-20 06:58:49 +0300

    Updates

---
+++ precache-manifest.914d765935b6660da1a72b2a484ccb2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187da3f704e06e05a6ddc3ff67b9b359",
+    "url": "/farseer-redux/index.html"
+  },
+  {
+    "revision": "b31ab9c0b5b332932ade",
+    "url": "/farseer-redux/static/css/main.e3470f70.chunk.css"
+  },
+  {
+    "revision": "2935aeea792e3982f079",
+    "url": "/farseer-redux/static/js/2.57ccfafa.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/farseer-redux/static/js/2.57ccfafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31ab9c0b5b332932ade",
+    "url": "/farseer-redux/static/js/main.65a15134.chunk.js"
+  },
+  {
+    "revision": "9f3f74a3463c2fb6d5bc",
+    "url": "/farseer-redux/static/js/runtime-main.3ebb1a5e.js"
+  }
+]);
